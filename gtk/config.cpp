f3c85b455ae7d7771a750bffd8544d8b6b8d1bbd
tc.17 <tc.17@yandex.ru>
2013-08-29 22:41:14 +0400

    Phase 0: WIP

---
+++ gtk/config.cpp
@@ -0,0 +1,30 @@
+/*
+ * configuration routines
+ */
+
+#include <string>
+#include "solvespace.h"
+
+void CnfFreezeString(std::string& str, const char *name)
+{ 
+}
+
+void CnfFreezeDWORD(DWORD v, const char *name)
+{
+}
+
+void CnfFreezeFloat(float v, const char *name)
+{
+}
+
+std::string CnfThawString(const char *name)
+{
+}
+
+DWORD CnfThawDWORD(DWORD v, const char *name)
+{
+}
+
+float CnfThawFloat(float v, const char *name) 
+{
+}
---
+++ gtk/unixutil.cpp
@@ -0,0 +1,58 @@
+#include "solvespace.h"
+#include <set>
+
+static std::set<void *> pool;
+
+void *AllocTemporary(int n)
+{
+	void *p = malloc(n);
+	if (!p)
+		oops();
+
+	pool.insert(p);
+	return p;
+}
+
+void FreeTemporary(void *p) 
+{
+	pool.erase(p);
+	free(p);
+}
+
+void FreeAllTemporary(void)
+{
+	std::set<void *>::const_iterator it;
+	for (it = pool.begin(); it != pool.end(); ++it)
+		free(*it);
+
+	pool.clear();
+}
+
+void *MemRealloc(void *p, int n)
+{
+	p = realloc(p, n);
+	if (!p)
+		oops();
+	return p;
+}
+
+void *MemAlloc(int n)
+{
+	void *p = malloc(n);
+	if (!p)
+		oops();
+	return p;
+}
+
+void MemFree(void *p)
+{
+	free(p);
+}
+
+void dbp(const char *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    va_end(ap);
+}
